5ce3f85d42294116883a23e9ad31037159ae792d
fseemann <fseemann@mail.de>
2019-02-13 21:52:48 +0100

    Use module path

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module manic.skeleton {
+    requires kotlin.argparser;
+    requires kotlin.stdlib;
+    requires kotlinx.serialization.runtime;
+    requires freemarker;
+
+    exports com.manic.skeleton;
+}
